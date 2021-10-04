78e8e962cdaae9c5a8a66ca420ec149ffa1a673f
Chris Kapusta <ckapusta@outlook.com>
2021-10-04 10:51:19 -0500

    Create Add New Page

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -25,11 +25,22 @@ namespace OdeToFood.Pages.Restaurants
             this.htmlHelper = htmlHelper;
         }
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaturantData.GetById(restaurantId);
-            if(Restaurant == null)
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaturantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+
+
+                Restaurant = new Restaurant();
+
+
+            }
+           if(Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
 
